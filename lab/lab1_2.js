b010c5bccc440af59cb432657dd6695dbf555a7f
Raf-Mac475 <115108947+Raf-Mac475@users.noreply.github.com>
2022-12-15 13:31:14 +0100

    lab2

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -4,7 +4,27 @@ var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json');
 
 var task =  function(request, callback){
+	//Pobierz info o instancji ec2
+	const ec2= new AWS.EC2();
+	ec2.describeInstances((err, data) => {
+		console.log('describeInstances');
+		if (err) {
+			console.error(err);
+		} else
+		{
+			//Wyświetlanie info o instancji EC2
+			console.log(data);
+		}
+	})
 	
+	var params = {
+  InstanceIds: [
+     "i-09676e297268a3087"
+  ]
+ };
+ ec2.startInstances(params, function(err, data) {
+   if (err) console.log(err, err.stack); // an error occurred
+   else     console.log(data);           // successful response
 	
 }
 
